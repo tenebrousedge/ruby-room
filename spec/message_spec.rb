c4b9c58fd21c676bd18b952fdb4e05fba06e1a38
nkls <niklas@long.ch>
2017-05-17 10:08:37 -0700

    add integration testing

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -4,7 +4,7 @@ describe "Message" do
   describe '#escape_chars' do
     it 'tests message content for certain chars and replaces them with escaped chars' do
       message = Message.create(content: "<script>world</script>", user_id: nil)
-      user = User.create(username: "tanner", password_digest: "test")
+      user = User.create(username: "B.B. King", password_digest: "Lucille")
       user.messages << message
       expect(Message.all[0].content).to eq "&#60script&#62world&#60&#47script&#62"
     end
