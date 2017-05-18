0899103947d006c297b5e2a161b3a50805961d4a
seanpierce <contact@seanpierce.us>
2017-05-18 11:17:58 -0700

    edit README and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,8 @@
 require './lib/user'
 
 User.create(
-:username => 'seanpierce',
-:password => 'test',
-:profile_picture => "/img/no-profile-picture.jpg",
-:about_me => "no description available",
-:address => 0,
-:admin => true
-)
-User.create(
-:username => 'ken',
-:password => 'aloha',
-:profile_picture => "/img/no-profile-picture.jpg",
-:about_me => "no description available",
-:address => 0,
-:admin => true
-)
-User.create(
-:username => 'nik',
-:password => 'nik',
-:profile_picture => "/img/no-profile-picture.jpg",
-:about_me => "no description available",
-:address => 0,
-:admin => true
-)
-User.create(
-:username => 'tanner',
-:password => 'tanner',
+:username => 'admin',
+:password => 'admin',
 :profile_picture => "/img/no-profile-picture.jpg",
 :about_me => "no description available",
 :address => 0,
