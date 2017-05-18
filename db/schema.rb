b9556ba3f1a525a0072785b1ecaab2892f1c0de6
seanpierce <contact@seanpierce.us>
2017-05-18 10:34:15 -0700

    admin functionality only accessible to admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518000751) do
+ActiveRecord::Schema.define(version: 20170518170840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170518000751) do
     t.string "profile_picture"
     t.string "about_me"
     t.string "address"
+    t.boolean "admin"
   end
 
 end
