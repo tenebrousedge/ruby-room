ed62122b3ddb3cce6fdfca29218a61115f72d871
Kai Leahy <canhascodez@gmail.com>
2017-05-19 07:24:24 -0700

    Update hash_mash.rb
    `.each` is only appropriate when the loop is being used for its side effect (writing to a file, e.g.), not for manipulating the elements of an array. 
    
    This code properly belongs in the ActiveRecord models. Active users should be a scope on the user model, and the other method should be defined on the message model.

--- lib/hash_mash.rb
+++ lib/hash_mash.rb
@@ -1,29 +1,16 @@
 class HashMash
   def self.mash_the_hash(last_message_id)
-    mashed_hash_a = []
-    Message.all.each do |message|
-      user = User.find(message.user_id)
-      if  message.id > last_message_id.to_i
-        mashed_hash_a.push({
-          :id => message.id,
-          :content => message.content,
-          :display_time => message.display_time,
-          :username => user.username,
-          :profile_picture => user.profile_picture,
-          :about_me => user.about_me
-        })
-      end
-    end
-    mashed_hash_a
+    Message.all
+      .select { |e| e > last_message_id.to_i }
+      .map { |e| 
+        { id: e.id,
+          content: e.content,
+          display_time: e.display_time,
+          profile_picture: e.user.profile_picture
+          about_me: e.user.about_me }}
   end
 
   def self.active_users
-    active_users = []
-    User.all.each do |user|
-      if user.activity == true
-        active_users.push(user)
-      end
-    end
-    active_users.sort { |x,y| x.username <=> y.username }
+    User.all.select { |e| e.activity }.sort_by &:username
   end
 end
